efa5431a93d8d8e1c667d69d19a7b6e00e8a03b4
Cokiiii <2538466160@qq.com>
2025-10-16 22:05:51 +0800

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import gsap from 'gsap';
+import {ScrollTrigger,SplitText} from 'gsap/all'
+const App = () => {
+  return (
+    <div className='flex-center h-[100vh]'>
+        <h1 className='text-3xl text-indigo-300'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
